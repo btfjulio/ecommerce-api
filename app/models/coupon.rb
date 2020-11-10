8e38542fc59767b8587e6cae7ef3928bdf9fe89f
Bruno Tostes <btfjulio@hotmail.com>
2020-11-10 10:24:02 -0300

    add future date validator to coupons

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,7 +1,9 @@
 class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: {case_sensitive: false}
   validates :status, presence: true
-  validates :due_date, presence: true
+  validates :due_date, 
+            presence: true,
+            future_date: true
   validates :discount_value, 
             presence: true, 
             numericality: { greater_than: 0 }
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Coupon, type: :model do
   it { is_expected.to validate_presence_of(:discount_value) }
   it { is_expected.to validate_numericality_of(:discount_value).is_greater_than(0) }
 
+  it "expected to validate that due_date cannot be before current date" do
+    subject.due_date = 1.day.ago
+    expect(subject.valid?).to eq(false) 
+    expect(subject.errors.keys).to include(:due_date) 
+  end
+
+  it "expected to validate that due_date cannot be equal current date" do
+    subject.due_date = Time.zone.now
+    expect(subject.valid?).to eq(false) 
+    expect(subject.errors.keys).to include(:due_date) 
+  end
+
+  it "expected to validate that due_date after current date" do
+    subject.due_date = Time.zone.now
+    expect(subject.errors.keys).not_to include(:due_date) 
+  end
+
 end

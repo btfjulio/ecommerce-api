497ee76ede56a448116575bf00df21161971535b
Bruno Tostes <btfjulio@hotmail.com>
2020-11-13 15:56:33 -0300

    add validation to categories requests

--- spec/shared_examples/forbidden_access_example.rb
+++ spec/shared_examples/forbidden_access_example.rb
@@ -1,6 +1,6 @@
 shared_examples "forbidden access" do
   it "returns error message" do
-    expoect(body_json['errors']['message']).to eq('Forbiden access')
+    expect(body_json['errors']['message']).to eq('Forbidden access')
   end
 
   it 'return forbidden status' do

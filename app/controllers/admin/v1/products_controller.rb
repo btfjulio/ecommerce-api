3d2cffc6074ff3184a44a81f98b3145e282d1dbf
Bruno Tostes <btfjulio@hotmail.com>
2020-12-09 10:28:57 -0300

    add service to action call on product controller

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -6,25 +6,35 @@ module Admin::V1
       @products = set_products
     end
 
-    def show; end
-
     def create
-      @product = Product.new(product_params)
-      save_product!
+      run_service
+    rescue Admin::ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
     end
-
+    
+    def show; end
+    
     def update
-      @product.attributes = product_params
-      save_product!
+      run_service
+    rescue Admin::ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
     end
 
     def destroy
+      @product.productable.destroy!
       @product.destroy!
-    rescue StandardError
-      render_error(fields: @product.errors.messages)
+    rescue ActiveRecord::RecordNotDestroyed
+      render_error(fields: @product.errors.messages.merge(@product.productable.errors.messages))
     end
 
     private
+
+    def run_service(product = nil)
+      @saving_service = Admin::ProductSavingService.new(product_params.to_h, @product)
+      @saving_service.call
+      @product = @saving_service.product
+      render :show
+    end
 
     def set_product
       @product = Product.find(params[:id])
@@ -33,10 +43,22 @@ module Admin::V1
     def product_params
       return {} unless params.has_key?(:product)
 
-      params.require(:product).permit(
+      permitted_params = params.require(:product).permit(
         :name, :image, :price, :status,
         :description, :price, :productable, category_ids: []
       )
+
+      permitted_params.merge(productable_params)
+    end
+
+    def productable_params
+      productable_type = params[:product][:productable] || @product&.productable_type&.underscore
+      return unless productable_type.present?
+      send("#{productable_type}_params")
+    end
+    
+    def game_params
+      params.require(:product).permit(:mode, :release_date, :developer, :system_requirement_id)
     end
 
     def set_products
